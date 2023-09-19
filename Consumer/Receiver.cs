301ec452b1fb239d0f064547b8ea9d3620338015
Suhail Farooqi <suhail.farooqi@datamatics.com>
2023-09-20 02:47:43 +0530

    Add project files.

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+public class Receiver
+{
+    private static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "mashq",
+            Port = 5672
+        };
+
+        using var connection = factory.CreateConnection();
+
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare("BasicTest", false, false, false, null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.Span;
+                var message = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine(message);
+            };
+
+            channel.BasicConsume("BasicTest", true, consumer);
+        }
+
+        Console.WriteLine("Press [enter] to exit the Sender App.");
+        Console.ReadLine();
+    }
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Text;
+
+using RabbitMQ.Client;
+
+public class Sender
+{
+    private static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "mashq",
+            Port = 5672
+        };
+
+        using var connection = factory.CreateConnection();
+
+        using(var channel  = connection.CreateModel())
+        {
+            channel.QueueDeclare("BasicTest",false,false,false,null);
+
+            var message = "Getting started with dotnet and rabbitMq.";
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish("", "BasicTest", null, body);
+
+            Console.WriteLine("Sent message: {0}", message);
+        }
+
+        Console.WriteLine("Press [enter] to exit the Sender App.");
+        Console.ReadLine();
+    }
+}
